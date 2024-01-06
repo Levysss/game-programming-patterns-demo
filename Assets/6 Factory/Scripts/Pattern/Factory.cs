e5a75c42117489c64a90064a89d21da8bfbb4976
Matheus Levy <srlevy008@gmail.com>
2024-01-06 14:39:56 -0300

    reanaleze

--- Assets/6 Factory/Scripts/Pattern/Factory.cs
+++ Assets/6 Factory/Scripts/Pattern/Factory.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 namespace DesignPatterns.Factory
 {
-    // base class for factories
 public abstract class Factory : MonoBehaviour
 {
     public abstract IProduct GetProduct(Vector3 position);
-
-    // shared method with all factories
     public string GetLog(IProduct product)
     {
         string logMessage = "Factory: created product " + product.ProductName;
         return logMessage;
+        
     }
 }
 }
--- Assets/6 Factory/Scripts/Pattern/IProduct.cs
+++ Assets/6 Factory/Scripts/Pattern/IProduct.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 namespace DesignPatterns.Factory
 {
-    // a common interface between products
     public interface IProduct
     {
-        // add common properties and methods here
         public string ProductName { get; set; }
-
-        // customize this for each concrete product
         public void Initialize();
     }
 }
